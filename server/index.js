86d43f9542c9267d566811fbcea452bb7250f4ce
Daniel YL YIP <2602505y@student.gla.ac.uk>
2022-03-24 11:39:17 +0000

    set up basic route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import postRoute from './routes/posts.js'
 
 //set up app
 const app = express();
 app.use(express.json({limit: "15mb", extended: "true"}));
 app.use(express.urlencoded({limit:"15mb", extended: "true"}));
 app.use(cors());
+
+    //link routes
+    app.use('/posts', postRoute);
 
 //connect to DB
 const MONGODB_URL = "mongodb+srv://admin-daniel:Danielyip@cluster0.iqbyh.mongodb.net/memoriesPractice?retryWrites=true&w=majority"
