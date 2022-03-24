866a16d3d0e34b8bf623e1c42127955b9ddab0d3
Daniel YL YIP <2602505y@student.gla.ac.uk>
2022-03-24 11:33:06 +0000

    set up app, connection to DB

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const App = () =>{
     return (
-        <>
+        <div>
             <h1>App</h1>
-        </>
+        </div>
     )
 }
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -2,3 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+
+//set up app
+const app = express();
+app.use(express.json({limit: "15mb", extended: "true"}));
+app.use(express.urlencoded({limit:"15mb", extended: "true"}));
+app.use(cors());
+
+//connect to DB
+const MONGODB_URL = "mongodb+srv://admin-daniel:Danielyip@cluster0.iqbyh.mongodb.net/memoriesPractice?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 3000;
+mongoose.connect(MONGODB_URL)
+    .then( ()=>app.listen(PORT, ()=>console.log("Server started on "+PORT)) )
+    .catch( err => console.log(err) )
